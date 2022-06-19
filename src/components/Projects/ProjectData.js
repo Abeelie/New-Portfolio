413c3abd7ed4f9bb57d7d89449845c3f09a7fbe9
Abeelie <abrahaelill@gmail.com>
2022-06-19 11:34:22 -0400

    adding another project

--- src/components/Projects/ProjectData.js
+++ src/components/Projects/ProjectData.js
@@ -9,84 +9,92 @@ import BooksImage from "../../assets/Projects/BooksImage.png";
 import WeatherImage from "../../assets/Projects/WeatherImage.jpg";
 import SpaceImage from "../../assets/Projects/Space.jpg";
 import AncientEgyptImage from "../../assets/Projects/AncientEgypt.jpg";
+import IonicAppImage from "../../assets/Projects/IonicAppImage.png";
 
 const Data = [
     {
         image: AIStudyImage,
         alt: "AI",
         title: "AI Study",
-        description: "AI-Study is a platform dedicated to providing students with the tools they need to have efficient study sessions. Technologies used are React JS, Node JS, Express Rest API, PostgreSQL, Material UI, Speech Recognition, HTML and CSS.",
+        description: "AI-Study is a platform dedicated to providing students with the tools they need to have efficient study sessions. Technologies used were React JS, Node JS, Express Rest API, PostgreSQL, Material UI, Speech Recognition, HTML and CSS.",
         link: "https://ai-study.netlify.app/",
     },
     {
         image: BibleImage,
         alt: "bible",
         title: "Multilingual Bible API",
-        description: "I created and published a multilingual bible API that has translations for English, French, Spanish, and Chinese. The API can be found on Rapid API. Technologies used are Node JS, Express JS, MongoDB, Mongoose, and Express Rate Limiter.",
+        description: "I created and published a multilingual bible API that has translations for English, French, Spanish, and Chinese. The API can be found on Rapid API. Technologies used were Node JS, Express JS, MongoDB, Mongoose, and Express Rate Limiter.",
         link: "https://rapidapi.com/abrahaelill-GRIsGuS-TSY/api/multilingual-bible/",
     },
     {
         image: GuitarImage,
         alt: "Guitar",
         title: "Guitar Chords API",
-        description: "I created and published a Guitar Chords API where a user can get the finger position of a guitar chord. Technologies used are Node JS, Express JS, Mongoose, MongoDB, Swagger JS, and Express Rate Limiter.",
+        description: "I created and published a Guitar Chords API where a user can get the finger position of a guitar chord. Technologies used were Node JS, Express JS, Mongoose, MongoDB, Swagger JS, and Express Rate Limiter.",
         link: "https://rapidapi.com/abrahaelill-GRIsGuS-TSY/api/guitar-chords",
     },
     {
         image: LottoImage,
         alt: "Lotto",
         title: "Lotto API",
-        description: "I published a Powerball and Mega Millions API. The API gets statistical data on the Powerball and Mega Millions lottery data. The API can be found on Rapid API. Technologies used are Node JS, Express JS, MongoDB, Mongoose, Cheerio, Cron jobs and Express Rate Limiter.",
+        description: "I published a Powerball and Mega Millions API. The API gets statistical data on the Powerball and Mega Millions lottery data. The API can be found on Rapid API. Technologies used were Node JS, Express JS, MongoDB, Mongoose, Cheerio, Cron jobs and Express Rate Limiter.",
         link: "https://rapidapi.com/abrahaelill-GRIsGuS-TSY/api/powerball-and-mega-millions/",
     },
     {
         image: MathImage,
         alt: "Math",
         title: "Math API",
-        description: "I created and published a Math API that can be used to solve arithmetic, geometry, number theory, statistics, fractions problems and much more. Technologies used are Node JS, Express JS, Node Cron jobs and Express Rate Limiter.",
+        description: "I created and published a Math API that can be used to solve arithmetic, geometry, number theory, statistics, fractions problems and much more. Technologies used were Node JS, Express JS, Node Cron jobs and Express Rate Limiter.",
         link: "https://rapidapi.com/abrahaelill-GRIsGuS-TSY/api/math7/",
     },
     {
         image: MemeImage,
         alt: "Meme",
         title: "Meme Central",
-        description: "Meme Central shows real time financial data on meme stocks and meme crypto. Technologies used are Python, Flask, HTML, CSS, JavaScript, JQuery, and API's.",
+        description: "Meme Central shows real time financial data on meme stocks and meme crypto. Technologies used were Python, Flask, HTML, CSS, JavaScript, JQuery, and API's.",
         link: "https://meme-central123.herokuapp.com/",
     },
     {
         image: MusicImage,
         alt: "Music",
         title: "Music History",
-        description: "Music History is a site dedicated to providing the public information on the history of music. Ranging from the different types of genres, artists and songs. Technologies used are Node JS, HTML, CSS, and API's.",
+        description: "Music History is a site dedicated to providing the public information on the history of music. Ranging from the different types of genres, artists and songs. Technologies used were Node JS, HTML, CSS, and API's.",
         link: "https://music-history101.herokuapp.com/",
     },
     {
         image: BooksImage,
         alt: "Books",
         title: "Good Books",
-        description: "Good Books provides free tools that help you find books or discover new ones. The site offers book searching tools and book recommendations. Technologies used are Node JS, HTML, CSS, and API's.",
+        description: "Good Books provides free tools that help you find books or discover new ones. The site offers book searching tools and book recommendations. Technologies used were Node JS, HTML, CSS, and API's.",
         link: "https://good-books-app.herokuapp.com/",
     },
     {
         image: WeatherImage,
         alt: "weather",
         title: "Weather Chrome Extension",
-        description: "A weather chrome extension that allows users to search for weather data for cities. Technologies used are React JS, TypeScript, Webpack, Chrome API, and Open Weather API.",
+        description: "A weather chrome extension that allows users to search for weather data for cities. Technologies used were React JS, TypeScript, Webpack, Chrome API, and Open Weather API.",
         link: "https://github.com/Abeelie/Chrome-Weather-Extension"
     },
     {
         image: SpaceImage,
         alt: "WordPress React Graphql",
         title: "Decoupled WordPress with React and GraphQL",
-        description: "A decoupled WordPress with React and GraphQL website that uses WordPress as a data source. Technologies used are React JS, WordPress, GraphQL, Bulma CSS and Apollo Client.",
+        description: "A decoupled WordPress with React and GraphQL website that uses WordPress as a data source. Technologies used were React JS, WordPress, GraphQL, Bulma CSS and Apollo Client.",
         link: "https://wordpress-graphql-react-demo.netlify.app"
     },
     {
         image: AncientEgyptImage,
         alt: "Ancient Egypt",
         title: "Ancient Egypt",
-        description: "A decoupled Strapi CMS with Next JS and GraphQL website that uses Strapi CMS as a data source. Technologies used are Next JS, Strapi CMS, GraphQL, React-Bootstrap and Apollo Client.",
+        description: "A decoupled Strapi CMS with Next JS and GraphQL website that uses Strapi CMS as a data source. Technologies used were Next JS, Strapi CMS, GraphQL, React-Bootstrap and Apollo Client.",
         link: "https://ancient-egypt-b2ihhpdpl-abeelie.vercel.app/"
+    },
+    {
+        image: IonicAppImage,
+        alt: "Eth Gas Tracker",
+        title: "Eth Gas Tracker Demo (Please Adjust window size to mobile to view app)",
+        description: "An Ethereum gas tracker app that lets you view an estimate on current gas fees, schedule gas fees alerts, and view market data on ethereum tokens. Technologies used were Ionic Framework, React JS and Strapi",
+        link: "https://eth-gas-fees-tracker.netlify.app/login"
     }
 ]
 
